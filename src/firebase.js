4faf1d42e8d323d0881175d9492c97c568941f44
Vaishnav <vaishnavshalikumar49@gmail.com>
2026-03-15 10:53:04 +0530

    New Changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyD8JGkwrGyA88Rb7s6R_FUTY3Atmxm5kKk",
   authDomain: "careguru-7d6b3.firebaseapp.com",
   projectId: "careguru-7d6b3",
 };
